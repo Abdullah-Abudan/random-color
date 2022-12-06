e135c86390bd30604cb8192c2cf57dfb7fbce0d0
Abdullah A. Abudan <abdullah-dan@outlook.com>
2022-12-06 15:58:33 +0200

    Set Interval and Clear Interval (optional) were added, and some names and properties were modified

--- js/main.js
+++ js/main.js
@@ -1,29 +1,48 @@
-let hex = document.getElementById("hex");
-let fill = document.getElementById("fill");
+let hexInput = document.getElementById("hex-input");
+let backgroundDiv = document.getElementById("background-div");
 let btn = document.getElementById("btn");
 
 btn.onclick = function getNewColor() {
   let symbols, color;
-  //These symbols or values for hexadecimal system
+  //These symbols or values for hexdecimal system
   symbols = "0123456789ABCDEF";
   color = "#";
   //The color consists of 6 digits
   for (let i = 0; i <= 5; i++) {
     color = color + symbols[Math.floor(Math.random() * 16)];
   }
-  // color as value for fill background
-  fill.style.background = color;
-  // color as value for hex element
-  hex.value = color;
+  // color as value for background-div background
+  backgroundDiv.style.background = color;
+  // color as value for hex-input element
+  hexInput.value = color;
 };
-hex.oninput = function getNewColorByInput() {
-  fill.style.background = hex.value;
+
+hexInput.oninput = function getNewColorByInput() {
+  backgroundDiv.style.background = hexInput.value;
 };
+
 // Add color when making a hover
-fill.onmouseover = function getNewColorByMouseover() {
-  fill.style.background = "#208790";
+function valueColor() {
+  let symbols, color;
+  symbols = "0123456789ABCDEF";
+  color = "#";
+  for (let i = 0; i <= 5; i++) {
+    color = color + symbols[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+let setClear;
+backgroundDiv.onmouseover = function getNewColorByMouseover() {
+  setClear = setInterval(function () {
+    backgroundDiv.style.background = valueColor();
+    hexInput.value = valueColor();
+  }, 500);
 };
+
 // Remove the color when removing the hover
-fill.onmouseout = function getNewColorByMouseout() {
-  fill.style.background = hex.value;
+backgroundDiv.onmouseout = function getNewColorByMouseout() {
+  clearInterval(setClear);
+  backgroundDiv.style.background = "";
+  hexInput.value = "#800080";
 };
